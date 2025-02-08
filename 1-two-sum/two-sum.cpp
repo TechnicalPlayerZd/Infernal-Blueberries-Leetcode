05bab64d9deb814a2f4d31f5c473c15681571a06
Infernal_Blueberry <alfredtonyconnect@gmail.com>
2025-02-08 19:55:56 +0900

    Time: 0 ms (100.00%) | Memory: 14.8 MB (35.34%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,26 +1,19 @@
+#include <iostream>
+#include <set>
 
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // Create an unordered_map to store numbers and their corresponding indices
-        std::unordered_map<int, int> numToIndexMap;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashmap;
 
-        // Loop through the array
-        for (int i = 0; i < nums.size(); i++) {
-            // Calculate the difference between the target and the current number
-            int diff = target - nums[i];
-
-            // Check if the difference already exists in the map
-            if (numToIndexMap.find(diff) != numToIndexMap.end()) {
-                // If it exists, return the indices of the current number and the number that adds up to the target
-                return {i, numToIndexMap[diff]};
+        for (int i =0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hashmap.find(complement) != hashmap.end()) {
+                return {hashmap[complement], i};
             }
-
-            // If it doesn't exist, add the current number and its index to the map
-            numToIndexMap[nums[i]] = i;
+            hashmap[nums[i]] = i;
         }
-
-        // If no two numbers add up to the target, return an empty vector
         return {};
+
     }
 };
