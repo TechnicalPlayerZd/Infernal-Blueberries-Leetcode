db83e1b404b17e0224b9cd9453e49fca059c5b4a
Infernal_Blueberry <alfredtonyconnect@gmail.com>
2025-03-12 18:36:42 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (22.96%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,10 +1,20 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        ans = [0,0]
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                ans[0] +=1
-            if nums[i] < 0:
-                ans[1] +=1
-        return max(ans)
+        neg_count = self.binary_search(nums, 0) 
+        pos_count = len(nums) - self.binary_search(nums, 1)
+        return max(neg_count, pos_count)
+
+    def binary_search(self, nums, target):
+        left, right = 0, len(nums) - 1
+        result = len(nums)
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                result = mid
+                right = mid - 1
+        
+        return result
         
